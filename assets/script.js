43efa331597bb0da5472589fa6e98ee79bf73493
Ryan Adams <ryanadams14@gmail.com>
2021-06-07 23:49:44 -0500

    added moment conditional formatting and additional clear button

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,24 @@ $('#currentDay').text(currentDate);
 var biggest = Math.max.apply(Math, $('.input-group-text').map(function(){ return $(this).width(); }).get());
 $('.input-group-text').width(biggest);
 
-$(function timeColor() {
-    $('.content')
-})
+// format color based on what time it is
+var currentTime = moment().format('HH');
+
+$(document).ready(function(){
+    var idArr = [];
+        $(".content").each(function(){
+            idArr.push($(this).attr("id"));
+        });
+        for (var i = 0; i < idArr.length; i++) {
+            if (currentTime > idArr[i]){
+                $('.content').css("background-color", "#d3d3d3");
+            } if (currentTime == idArr[i]) {
+                $('.content').css("background-color", "#d2f8d2");
+            } if (currentTime < idArr[i]) {
+                $('.content').css("background-color", "#e1f1ff");
+            }
+        };
+});
+
+// save input to localStorage
+
